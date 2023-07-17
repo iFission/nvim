38408b778665afe1ea55ebd53129e62ad7fd2247
iFission <alexwwj@icloud.com>
2023-07-17 15:02:49 +0800

    refactor: organise keymap

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -1,28 +1,8 @@
--- Mapping data with "desc" stored directly by vim.keymap.set().
---
--- Please use this mappings table to set keyboard mapping since this is the
--- lower level configuration and more robust one. (which-key will
--- automatically pick-up stored data by this setting.)
 return {
-  -- first key is the mode
   n = {
-    -- second key is the lefthand side of the map
-    -- mappings seen under group name "Buffer"
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-    ["<leader>bD"] = {
-      function()
-        require("astronvim.utils.status").heirline.buffer_picker(
-          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
-        )
-      end,
-      desc = "Pick to close",
-    },
-    -- tables with the `name` key will be registered with which-key if it's installed
-    -- this is useful for naming menus
-    ["<leader>b"] = { name = "Buffers" },
-    -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
 
+    -- git
     ["<leader>g4"] = { "<cmd>Gitsign prev_hunk<cr>", desc = "Prev hunk" },
     ["<leader>g3"] = { "<cmd>Gitsign next_hunk<cr>", desc = "Next hunk" },
     ["g4"] = { "<cmd>Gitsign prev_hunk<cr>", desc = "Prev hunk" },
@@ -32,19 +12,26 @@ return {
     ["<leader>gD"] = { "<cmd>Gitsign reset_buffer<cr>", desc = "Reset buffer" },
     ["<leader>ga"] = { "<cmd>Gitsign stage_hunk<cr>", desc = "Stage hunk" },
     ["<leader>gA"] = { "<cmd>Gitsign stage_buffer<cr>", desc = "Stage buffer" },
+
+    -- find
     ["<leader>fr"] = { "<cmd> Telescope resume<cr>", desc = "Resume previous search" },
     ["<leader>fh"] = { "<cmd> Telescope oldfiles<cr>", desc = "Find history" },
     ["<leader>fH"] = { "<cmd> Telescope help_tags<cr>", desc = "Find help" },
+
+    -- editing/intellisense
     ["gi"] = { function() vim.lsp.buf.hover() end, desc = "Hover symbol details" },
     ["gp"] = { function() vim.diagnostic.open_float() end, desc = "Hover problems" },
     ["g2"] = { function() vim.diagnostic.goto_next() end, desc = "Next problem" },
     ["g1"] = { function() vim.diagnostic.goto_prev() end, desc = "Prev problem" },
     ["<leader>2"] = { function() require("telescope.builtin").lsp_definitions() end, desc = "Definition" },
     ["<leader>3"] = { function() require("telescope.builtin").commands() end, desc = "Commands" },
+
+    -- terminal
     ["<C-t>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
     ["<leader>tt"] = { "<cmd>ToggleTerm direction=float<cr>", desc = "ToggleTerm float" },
 
     -- buffer
+    ["<leader>b"] = { name = "Buffers" },
     ["<leader>x"] = { "<Cmd>BufferClose<CR>", desc = "Close buffer" },
     ["<leader>X"] = { "<Cmd>BufferRestore<CR>", desc = "Restore buffer" },
     ["H"] = { "<Cmd>BufferPrevious<CR>", desc = "Prev buffer" },
@@ -53,8 +40,7 @@ return {
     ["<leader>b>"] = { "<Cmd>BufferMoveNext<CR>", desc = "Buffer move next" },
   },
   t = {
+    -- terminal
     ["<C-t>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
-    -- setting a mapping to false will disable it
-    -- ["<esc>"] = false,
   },
 }
