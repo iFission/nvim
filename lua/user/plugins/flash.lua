8c01e27e4604dc4a2a603a045f96c0add2ae66ef
iFission <alexwwj@icloud.com>
2023-07-18 17:24:31 +0800

    feat: add flash continue

--- lua/user/plugins/flash.lua
+++ lua/user/plugins/flash.lua
@@ -2,51 +2,43 @@ return {
   "folke/flash.nvim",
   event = "VeryLazy",
   ---@type Flash.Config
-  opts = {
-    modes = {
-      char = {
-        jump_labels = true,
-      },
-    },
-  },
-  -- stylua: ignore
   keys = {
     {
       "s",
       mode = { "n", "x", "o" },
       function()
-        require("flash").jump({
+        require("flash").jump {
           search = {
-            mode = function(str)
-              return "\\<" .. str
-            end,
-
-          }
-        })
+            mode = function(str) return "\\<" .. str end,
+          },
+        }
       end,
-      desc = "Flash"
+      desc = "Flash",
     },
     {
       "<leader>s",
       mode = { "n", "o", "x" },
       function() require("flash").treesitter() end,
-      desc =
-      "Flash Treesitter"
+      desc = "Flash Treesitter",
     },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     {
       "R",
       mode = { "o", "x" },
       function() require("flash").treesitter_search() end,
-      desc =
-      "Treesitter Search"
+      desc = "Treesitter Search",
+    },
+    {
+      "<leader>fs",
+      mode = { "n", "x", "o" },
+      function() require("flash").jump { continue = true } end,
+      desc = "Flash continue",
     },
     {
       "<c-s>",
       mode = { "c" },
       function() require("flash").toggle() end,
-      desc =
-      "Toggle Flash Search"
+      desc = "Toggle Flash Search",
     },
   },
 }
