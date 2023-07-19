cb1f674c909a369a6613719fdf85acf90fb9252b
iFission <alexwwj@icloud.com>
2023-07-19 15:39:39 +0800

    feat: add telescope config

---
+++ lua/user/plugins/telescope.lua
@@ -0,0 +1,33 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = function()
+    local actions = require "telescope.actions"
+    local get_icon = require("astronvim.utils").get_icon
+    return {
+      defaults = {
+        prompt_prefix = get_icon("Selected", 1),
+        selection_caret = get_icon("Selected", 1),
+        path_display = { "truncate" },
+        sorting_strategy = "ascending",
+        layout_strategy = "flex",
+        layout_config = {
+          horizontal = { prompt_position = "top", preview_width = 0.6 },
+          vertical = { mirror = true, preview_height = 0.6, prompt_position = "top" },
+          flex = { flip_columns = 120 },
+          width = 0.87,
+          height = 0.80,
+          preview_cutoff = 0,
+        },
+        mappings = {
+          i = {
+            ["<S-Down>"] = actions.cycle_history_next,
+            ["<S-Up>"] = actions.cycle_history_prev,
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+          },
+          n = { q = actions.close },
+        },
+      },
+    }
+  end,
+}
