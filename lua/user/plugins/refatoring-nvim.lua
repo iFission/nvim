b49ad56b4a3fc02ffc5d08737cd3354b2d9b1f56
iFission <alexwwj@icloud.com>
2023-07-20 09:47:58 +0800

    feat: add refactoring

---
+++ lua/user/plugins/refatoring-nvim.lua
@@ -0,0 +1,19 @@
+return {
+  "ThePrimeagen/refactoring.nvim",
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-treesitter/nvim-treesitter" },
+  },
+  event = "BufEnter",
+  config = function()
+    require("refactoring").setup {}
+
+    require("telescope").load_extension "refactoring"
+    vim.keymap.set({ "n", "x" }, "<leader>rr", function() require("telescope").extensions.refactoring.refactors() end)
+
+    vim.keymap.set("n", "<leader>rp", function() require("refactoring").debug.printf { below = false } end)
+    vim.keymap.set({ "x", "n" }, "<leader>rv", function() require("refactoring").debug.print_var() end)
+    vim.keymap.set({ "x", "n" }, "<C-p>", function() require("refactoring").debug.print_var() end)
+    vim.keymap.set("n", "<leader>rc", function() require("refactoring").debug.cleanup {} end)
+  end,
+}
