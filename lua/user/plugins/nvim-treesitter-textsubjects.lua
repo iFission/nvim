e329fca31d619923b38bbc0302f57adcc07aaded
iFission <alexwwj@icloud.com>
2023-07-26 15:15:17 +0800

    feat: add nvim-treesitter-textsubjects

---
+++ lua/user/plugins/nvim-treesitter-textsubjects.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "RRethy/nvim-treesitter-textsubjects",
+    event = "BufEnter",
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        textsubjects = {
+          enable = true,
+          prev_selection = ",", -- (Optional) keymap to select the previous selection
+          keymaps = {
+            ["."] = "textsubjects-smart",
+            [";"] = "textsubjects-container-outer",
+            ["i:"] = "textsubjects-container-inner",
+          },
+        },
+      }
+    end,
+  },
+}
