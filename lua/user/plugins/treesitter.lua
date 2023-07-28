8dc7071ea54912d1342626aee9c2aed5c03fc0d9
iFission <alexwwj@icloud.com>
2023-07-28 21:53:29 +0800

    feat: add treesitter mason ensure installed

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "lua", "vim" },
+    ensure_installed = { "lua", "vim", "tsx", "javascript", "kotlin", "java", "yaml" },
   },
 }
