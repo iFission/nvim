cf8e77f7551e583554b2efb071a7306dff321044
iFission <alexwwj@icloud.com>
2023-07-31 17:38:34 +0800

    fix: change tree climber keymap

--- lua/user/plugins/tree-climber-nvim.lua
+++ lua/user/plugins/tree-climber-nvim.lua
@@ -5,8 +5,8 @@ return {
     -- enabled = false,
     config = function()
       local keyopts = { noremap = true, silent = true }
-      vim.keymap.set({ "n", "v", "o" }, "<Left>", require("tree-climber").goto_parent, keyopts)
-      vim.keymap.set({ "n", "v", "o" }, "<Right>", require("tree-climber").goto_child, keyopts)
+      vim.keymap.set({ "n", "v", "o" }, "<S-Left>", require("tree-climber").goto_parent, keyopts)
+      vim.keymap.set({ "n", "v", "o" }, "<S-Right>", require("tree-climber").goto_child, keyopts)
       vim.keymap.set({ "n", "v", "o" }, "<Down>", require("tree-climber").goto_next, keyopts)
       vim.keymap.set({ "n", "v", "o" }, "<Up>", require("tree-climber").goto_prev, keyopts)
       vim.keymap.set({ "v", "o" }, "in", require("tree-climber").select_node, keyopts)
