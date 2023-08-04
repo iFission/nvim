96171f7b1cd267f51680a08c72b500ab9e6d0460
iFission <alexwwj@icloud.com>
2023-08-04 15:31:03 +0800

    fix: add config for neoclip

--- lua/user/plugins/nvim-neoclip.lua
+++ lua/user/plugins/nvim-neoclip.lua
@@ -3,6 +3,11 @@ return {
   event = "VeryLazy",
   dependencies = {
     { "nvim-telescope/telescope.nvim" },
+    { "kkharji/sqlite.lua",           module = "sqlite" },
   },
-  setup = function() require("neoclip").setup() end,
+  config = function()
+    require("neoclip").setup { enable_persistent_history = true, default_register = "+" }
+    require("telescope").load_extension "neoclip"
+    vim.keymap.set({ "n" }, "<leader>8", "<cmd>Telescope neoclip<cr>")
+  end,
 }
