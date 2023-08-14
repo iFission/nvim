6704a604b0b68f078bb3f70c3e31b3cfe8e35783
iFission <alexwwj@icloud.com>
2023-08-14 16:54:27 +0800

    feat: add better escape

---
+++ lua/user/plugins/better-escape-nvim.lua
@@ -0,0 +1,10 @@
+return {
+  "max397574/better-escape.nvim",
+  lazy = false,
+  opts = {
+    mapping = { "jk", "jj", "kk", "kj", "eu", "ue" }, -- a table with mappings to use
+    timeout = vim.o.timeoutlen,                       -- the time in which the keys must be hit in ms. Use option timeoutlen by default
+    clear_empty_lines = false,                        -- clear line after escaping if there is only whitespace
+    keys = "<Esc>",                                   -- keys used for escaping, if it is a function will use the result everytime
+  },
+}
