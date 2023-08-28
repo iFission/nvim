dd45b96935760d62990eabccd949468fb13004ae
iFission <alexwwj@icloud.com>
2023-08-28 14:54:04 +0800

    refactor: change telescope-diff event to key

--- lua/user/plugins/telescope-diff-nvim.lua
+++ lua/user/plugins/telescope-diff-nvim.lua
@@ -1,16 +1,14 @@
 return {
   "jemag/telescope-diff.nvim",
-  event = "BufEnter",
   dependencies = {
     { "nvim-telescope/telescope.nvim" },
   },
   init = function() require("telescope").load_extension "diff" end,
-  config = function()
-    vim.keymap.set(
-      "n",
+  keys = {
+    {
       "<leader>D",
       function() require("telescope").extensions.diff.diff_current { hidden = true } end,
-      { desc = "Compare file with current" }
-    )
-  end,
+      { desc = "Compare file with current" },
+    },
+  },
 }
