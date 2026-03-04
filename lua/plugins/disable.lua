3257e5309e333b92b3eb2bc787f6166ce091d515
iFission <alexwwj@icloud.com>
2026-03-04 16:15:28 +0800

    fix: remove disabled

--- lua/plugins/disable.lua
+++
@@ -1,6 +0,0 @@
-return {
-  { "goolord/alpha-nvim", enabled = false },
-  { "nvimdev/dashboard-nvim", enabled = false },
-  { "nvim-mini/mini.surround", enabled = false },
-  { "nvim-mini/mini.pairs", enabled = false },
-}
