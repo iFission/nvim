fa2a15053808fd93432d1666a68d587dff44a9e9
iFission <alexwwj@icloud.com>
2026-03-05 16:34:55 +0800

    fix: migrate to multicursor

--- lua/plugins/vim-visual-multi.lua
+++ lua/plugins/vim-visual-multi.lua
@@ -1,6 +1,7 @@
 return {
   "mg979/vim-visual-multi",
   lazy = false,
+  enabled = false,
   config = function()
     vim.g.VM_default_mappings = 0
     vim.api.nvim_set_keymap("n", "<C-n>", "<Plug>(VM-Find-Under)", { noremap = true, silent = true })
