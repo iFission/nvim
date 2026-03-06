ecff3c7a1e0f26ce0546e916cf0ccf6526add352
iFission <alexwwj@icloud.com>
2026-03-06 13:32:31 +0800

    fix: update theme config

--- lua/plugins/bluloco.lua
+++ lua/plugins/bluloco.lua
@@ -1,9 +1,9 @@
 return {
-  'uloco/bluloco.nvim',
+  "uloco/bluloco.nvim",
   lazy = false,
   priority = 1000,
-  dependencies = { 'rktjmp/lush.nvim' },
+  dependencies = { "rktjmp/lush.nvim" },
   config = function()
-    -- your optional config goes here, see below.
+    vim.cmd("colorscheme bluloco")
   end,
 }
