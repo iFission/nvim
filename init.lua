687496ea2d4f1b50c771d508aad63bd905269e37
iFission <alexwwj@icloud.com>
2026-03-06 12:00:20 +0800

    feat: add osc52 clipboard copy

--- init.lua
+++ init.lua
@@ -1,6 +1,22 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = function()
+      return vim.split(vim.fn.getreg('"'), "\n")
+    end,
+    ["*"] = function()
+      return vim.split(vim.fn.getreg('"'), "\n")
+    end,
+  },
+}
+
 require("config.lazy")
 require("config.options")
 require("config.keymaps")
