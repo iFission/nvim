18f02777533425f44c12fdf10ef724761dd7a8d2
iFission <alexwwj@icloud.com>
2026-03-06 14:01:40 +0800

    fix: update auto-dark-mode

--- lua/plugins/auto-dark-mode.lua
+++ lua/plugins/auto-dark-mode.lua
@@ -1,18 +1,22 @@
 return {
   "f-person/auto-dark-mode.nvim",
   event = "VeryLazy",
+  cond = function()
+    local is_ssh = vim.env.SSH_CLIENT ~= nil or vim.env.SSH_TTY ~= nil or vim.env.SSH_CONNECTION ~= nil
+    return not is_root and not is_ssh
+  end,
   config = {
     update_interval = 1000,
     set_dark_mode = function()
       vim.api.nvim_set_option("background", "dark")
-      vim.cmd "colorscheme bluloco-dark"
+      vim.cmd("colorscheme bluloco-dark")
     end,
     set_light_mode = function()
       vim.api.nvim_set_option("background", "light")
       if vim.loop.os_uname().sysname == "Darwin" then
-        vim.cmd "colorscheme bluloco-light"
+        vim.cmd("colorscheme bluloco-light")
       else
-        vim.cmd "colorscheme bluloco-dark"
+        vim.cmd("colorscheme bluloco-dark")
       end
     end,
   },
