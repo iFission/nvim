95f70551028b5eb27745e456fc37d01fd59de8f3
iFission <alexwwj@icloud.com>
2026-03-13 15:13:16 +0800

    fix: migrate to markview

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -23,29 +23,10 @@ return {
     end,
   },
   {
-    "MeanderingProgrammer/render-markdown.nvim",
-    opts = {
-      code = {
-        sign = false,
-        width = "block",
-        right_pad = 1,
-      },
-      heading = {
-        sign = false,
-        icons = {},
-      },
-      checkbox = {
-        enabled = false,
-      },
-    },
+    "OXY2DEV/markview.nvim",
     ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
-    config = function(_, opts)
-      require("render-markdown").setup(opts)
-      Snacks.toggle({
-        name = "Render Markdown",
-        get = require("render-markdown").get,
-        set = require("render-markdown").set,
-      }):map("<leader>um")
-    end,
+    keys = {
+      { "<leader>um", "<cmd>Markview toggle<cr>", desc = "Toggle Markview" },
+    },
   },
 }
