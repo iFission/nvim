0e622b86e84a29d980ec016bc5e2206827e5156f
iFission <alexwwj@icloud.com>
2026-03-21 14:29:07 +0800

    fix: add keymap close lazynvim with esc

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -11,3 +11,11 @@ vim.api.nvim_create_autocmd("BufEnter", {
     vim.bo.readonly = false
   end,
 })
+
+-- close lazynvim with esc
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "lazy",
+  callback = function()
+    vim.keymap.set("n", "<Esc>", "<cmd>close<cr>", { buffer = true })
+  end,
+})
