a08ddd4de1468445e029ff7af11f9a1936347aea
iFission <alexwwj@icloud.com>
2026-03-31 14:40:10 +0800

    fix: update dropbar icon keymap

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -6,13 +6,22 @@ return {
       "nvim-telescope/telescope-fzf-native.nvim",
       build = "make",
     },
-    config = function()
-      require("dropbar").setup()
+    opts = {
+      icons = {
+        kinds = {
+          dir_icon = function(_)
+            return "", nil
+          end,
+        },
+      },
+    },
+    config = function(_, opts)
+      require("dropbar").setup(opts)
 
       local api = require("dropbar.api")
-      vim.keymap.set("n", "<Leader>.", api.pick, { desc = "Dropbar: pick symbol" })
-      vim.keymap.set("n", "[.", api.goto_context_start, { desc = "Dropbar: go to context start" })
-      vim.keymap.set("n", "].", api.select_next_context, { desc = "Dropbar: select next context" })
+      -- vim.keymap.set("n", "<Leader>.", api.pick, { desc = "Dropbar: pick symbol" })
+      -- vim.keymap.set("n", "[.", api.goto_context_start, { desc = "Dropbar: go to context start" })
+      -- vim.keymap.set("n", "].", api.select_next_context, { desc = "Dropbar: select next context" })
     end,
   },
 }
