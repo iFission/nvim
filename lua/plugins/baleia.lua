e904231ca2518ce5890a84d12fd1b6078ba66de0
iFission <alexwwj@icloud.com>
2026-04-20 13:16:13 +0800

    fix: add baleia

---
+++ lua/plugins/baleia.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "m00qek/baleia.nvim",
+    version = "*",
+    config = function()
+      vim.g.baleia = require("baleia").setup({})
+
+      -- Command to colorize the current buffer
+      vim.api.nvim_create_user_command("BaleiaColorize", function()
+        vim.g.baleia.once(vim.api.nvim_get_current_buf())
+      end, { bang = true })
+
+      vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+        pattern = "*.log",
+        callback = function()
+          vim.g.baleia.automatically(vim.api.nvim_get_current_buf())
+        end,
+      })
+
+      -- Command to show logs
+      vim.api.nvim_create_user_command("BaleiaLogs", vim.cmd.messages, { bang = true })
+    end,
+  },
+}
